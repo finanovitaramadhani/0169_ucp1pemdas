938f658466d38806913f7052c735000acffc8b3d
finanovita <finanovita479@gmail.com>
2024-05-05 17:12:49 +0700

    Add project files.

---
+++ UCP 1 Pemdas/UCP 1 Pemdas.cpp
@@ -0,0 +1,63 @@
+// UCP 1 Pemdas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// 1. 3 variable tersebut yaitu, string untuk teks, interger untuk  bilangan bulat, dan float untuk pecahan
+// 2. prosedur yaitu satu blok program yang digunakan untuk menghasilkan sesuatu tetapi tidak mengembalikan nilai dan hanya melewatkan nilai.
+//     dengan cara void nama.prosedure () {} 
+//    fungsi yaitu satu blok program yang digunakan untuk menghasilkan sesuatu dengan mengembalikan nilai
+//      implementasinya dengan cara 
+//      tipe.data nama fungsi () {
+//          return nilai yang dikembalikan
+//       {
+// 3. looping merupakan perulangan. terdapat jumlah perulangan sudah tertentu yang disebut FOR dan jumlah perulangan yang tidak diketahui
+//      dalam perulangan tidak diketahui terdapat 2 kondisi yaitu kondisi diuji didepan yang disebut WHILE dan kondisi diuji dibelakang
+//      yang disebut DO WHILE
+//      cara implementasikannya yaitu dengan cara
+//      FOR (kondisi.awal; kondisi akhir; pengubah kondisi) {
+//          proses yang diulang
+//      {
+//      WHILE (kondisi) {
+//          proses yang diulang
+//      {
+//      DO {
+//          proses yang diulang
+//      { WHILE (kondisi)
+// 4. if dan else
+// 5. struct yaitu untuk membuat tipe data yang baru
+
+#include <iostream>
+using namespace std;
+
+
+int indeks, nilai;
+string namakota;
+string status;
+
+void inputdata() {
+    cout << "Masukan ratarata indeks :";
+    cin >> indeks;
+    cout << "Masukan nilai sensor :";
+    cin >> nilai;
+}
+
+int total() {
+    return indeks status;
+}
+
+
+void display() {
+    cout << "statusnya adalah :" << total() << endl;
+   
+}
+
+
+int main()
+{
+    inputdata();
+    display();
+
+
+    
+}
+
+
